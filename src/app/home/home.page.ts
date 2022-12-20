d5d52c47f655079320084b734c168f93bad47e97
Amagaraï Guindo <amagarai6@gmail.com>
2022-12-20 03:03:55 +0000

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { AlertController } from '@ionic/angular';
+import { Configuration, OpenAIApi } from "openai";
 
 @Component({
   selector: 'app-home',
@@ -7,6 +11,56 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  apikeys  = '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'
+  endpoint = 'https://api.openai.com/v1/completions';
+  resultat:any;
+  loading : boolean = false;
+  verifie = '';
+  constructor(private http: HttpClient, private alertController: AlertController) {}
+
+  header = new HttpHeaders({
+    "Authorization" : "Bearer "+ this.apikeys ,
+  });
+
+  Submit(datas: NgForm){
+    if (this.verifie === '') {
+      this.presentAlert('','Champs vide')
+    } else {
+      this.loading = true
+      let data = {
+        prompt: datas.value.donnee,
+        model: 'text-davinci-003',
+        max_tokens: 2000,
+      };
+      this.http.post(this.endpoint, data, {headers: this.header}).subscribe((response: any) => {
+        if (response) {
+          this.loading = false;
+          let text = response;
+          this.verifie = ''
+          this.resultat = text.choices[0].text
+          datas.reset();
+        }
+      });
+    }
+  }
+
+  Ionchange(event: any){
+    this.verifie = event.target.value
+  }
+
+  translateToHtml = (text: string) => text.split("\n").map((str) =>`<p>${str}</p>`).join("");
+
+  async presentAlert(header : string, message : string) {
+    const alert = await this.alertController.create({
+      header: header,
+      // subHeader: 'Subtitle',
+      mode: 'ios',
+      cssClass: 'my-custom-class',
+      message: '<b>'+message+'</b>',
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
 
 }
